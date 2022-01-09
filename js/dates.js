c04077104d8ede017703d8f1461ad8026e33a460
shauniebyrne <byr21002@byui.edu>
2022-01-08 20:44:53 -0700

    Week 1 Assignment

---
+++ js/dates.js
@@ -0,0 +1,5 @@
+let d = new Date();
+let year = d.getFullYear();
+
+document.getElementById("current-year").textContent = year;
+document.getElementById("updated-date").textContent = document.lastModified;
