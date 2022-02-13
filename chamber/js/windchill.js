8707716e5795091f1fd38635114d59e340b3ee76
shauniebyrne <byr21002@byui.edu>
2022-02-12 21:10:04 -0700

    changes

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,16 +1,17 @@
-let temperature = parsFloat(document.querySelector('#temp').textContent);
-let speed = parsFloat(document.querySelector('#speed').textContent);
-let windChill = '';
+let temp = parsFloat(document.querySelector('#temp').value);
+let speed = parsFloat(document.querySelector('#speed').value);
+let windChill;
 
-if (temperature <= 50 && speed > 3) {
-    windChill = formula(temperature, speed);
-    windChill = `${windChill} &#176; F`
+if (temp <= 50 && speed > 3) {
+    windChill = calculation(temp, speed);
+    windChill = `${windChill} &#176; F`;
 } else {
     windChill = 'N/A';
-}
+};
 
 document.querySelector('#wind-chill').innerHTML = windChill;
 
-function formula (t, s) {
-    return Math.pow(35.74 + (0.6215 * t) - ((35.75 * s) **  0.16) + ((0.4275 * t * s) ** 0.16));
-}
+function calculation(t, s) {
+    let number = 35.74 + (0.6215 * t) - ((35.75 * s) **  0.16) + ((0.4275 * t * s) ** 0.16);
+    return Math.round(number);
+};
