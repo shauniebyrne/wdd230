09ca56c6bb3ac58763580bbd5b823c05e92f9421
shauniebyrne <byr21002@byui.edu>
2022-03-05 20:13:15 -0700

    changes

--- chamber/js/directorcardslist.js
+++ chamber/js/directorcardslist.js
@@ -6,42 +6,64 @@ fetch(requestURL)
         return response.json();
     })
     .then(function(jsonObject) {
-        const cards = jsonObject['directory'];
+        const cardsAndList = jsonObject['directory'];
 
-        cards.forEach(displayCards);
+        cardsAndList.forEach(displayCards);
+        cardsAndList.forEach(displayList);
     });
 
 function displayCards(card) {
-        //Create elements to add to the document
-        let section = document.createElement('section');
-        let h3 = document.createElement('h3');
-        let portrait = document.createElement('img');
-        let p1 = document.createElement('p');
-        let p2 = document.createElement('p');
-        let a = document.createElement('a');
+    //Create elements to add to the document
+    let section = document.createElement('section');
+    let portrait = document.createElement('img');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let a = document.createElement('a');
     
-        //Add text content to the directory cards
-        h3.textContent = `${card.name}`;
-        p1.textContent = `${card.address}`;
-        p2.textContent = `${card.phonenumber}`;
-        a.textContent = `${card.website}`;
+    //Add text content to the directory cards
+    p1.textContent = `${card.address}`;
+    p2.textContent = `${card.phonenumber}`;
+    a.textContent = `${card.website}`;
     
-        //Build image attributes by using setAttribute method for src, alt, and loading attribute values.
-        portrait.setAttribute('src', card.logo);
-        portrait.setAttribute('alt', `Logo for ${card.name}`);
-        portrait.setAttribute('loading', 'lazy');
-        a.setAttribute('href', card.website);
+    //Build image attributes by using setAttribute method for src, alt, and loading attribute values.
+    portrait.setAttribute('src', card.logo);
+    portrait.setAttribute('alt', `Logo for ${card.name}`);
+    portrait.setAttribute('loading', 'lazy');
+    a.setAttribute('href', card.website);
     
-        //Add/append the h2 and image to the section(card)
-        section.appendChild(h3);
-        section.appendChild(portrait);
-        section.appendChild(p1);
-        section.appendChild(p2);
-        section.appendChild(a);
+    //Add/append the h2 and image to the section(card)
+    section.appendChild(portrait);
+    section.appendChild(p1);
+    section.appendChild(p2);
+    section.appendChild(a);
         
     
-        //Add/append the section(card) to the div with the card class
-        document.querySelector('.cards').appendChild(section);
+    //Add/append the section(card) to the div with the card class
+    document.querySelector('.cards').appendChild(section);
 }
 
-//document.getElementById('card-button').addEventListener('click', );
+function displayList(list) {
+    //Create elements to add to the document
+    let section = document.createElement('section');
+    let h3 = document.createElement('h3');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let a = document.createElement('a');
+    
+    //Add text content to the directory cards
+    h3.textContent = `${list.name}`;
+    p1.textContent = `${list.address}`;
+    p2.textContent = `${list.phonenumber}`;
+    a.textContent = `${list.website}`;
+    a.setAttribute('href', list.website);
+    
+    //Add/append the h2 and image to the section(card)
+    section.appendChild(h3);
+    section.appendChild(p1);
+    section.appendChild(p2);
+    section.appendChild(a);
+        
+    
+    //Add/append the section(card) to the div with the card class
+    document.querySelector('.list').appendChild(section);
+}
