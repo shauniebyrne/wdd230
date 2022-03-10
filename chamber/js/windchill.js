5207c1eb57218814b35f120011bf7f404b3edcd2
shauniebyrne <byr21002@byui.edu>
2022-03-10 15:24:42 -0700

    week 10

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,17 +1,41 @@
-let temp = parseFloat(document.querySelector('#temp').textContent);
-let speed = parseFloat(document.querySelector('#speed').textContent);
-let windChill;
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Taber&units=imperial&appid=502854408fb5e148e3e7e1ca21917b63";
 
-if (temp <= 50 && speed > 3) {
-    windChill = calculation(temp, speed);
-    windChill = `${windChill} &#176; F`;
-} else {
-    windChill = 'N/A';
-};
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        //console.log(jsObject);
 
-document.querySelector('#wind-chill').innerHTML = windChill;
+        // Get temp and speed from weather api
+        let temp = jsObject.main.temp;
+        let speed = jsObject.wind.speed;
+        let windChill;
 
-function calculation(t, s) {
-    let number = 35.74 + (0.6215 * t) - ((35.75 * s) **  0.16) + ((0.4275 * t * s) ** 0.16);
-    return Math.round(number);
-};
+        // If/Then for HTML display for calulated wind chill
+        if (temp <= 50 && speed > 3) {
+             windChill = calculation(temp, speed);
+             windChill = `${windChill} &#176;F`;
+        } else {
+             windChill = 'N/A';
+        };
+
+        // Wind Chill calculation
+        function calculation(t, s) {
+            let number = 35.74 + (0.6215 * t) - ((35.75 * s) **  0.16) + ((0.4275 * t * s) ** 0.16);
+            return Math.round(number);
+        };
+
+        // Add to the DOM
+        document.querySelector('#temp').textContent = temp.toFixed();
+        document.querySelector('#humidity').textContent = `${jsObject.main.humidity}%`;
+        document.querySelector('#speed').textContent = speed.toFixed(1);
+        document.querySelector('#wind-chill').innerHTML = windChill;
+
+        // Adding the current weather conditions image and info to the DOM
+        const iconImg = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+        const description = jsObject.weather[0].description;
+
+        document.querySelector('#condition-img').setAttribute('src', iconImg);
+        document.querySelector('#condition-img').setAttribute('alt', description);
+        document.querySelector('#current-conditions').textContent = description;
+        
+    });  
