5b7067b182bade5c6573eeccf992814eecd3445c
shauniebyrne <byr21002@byui.edu>
2022-03-10 22:29:33 -0700

    weather fixes

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,9 +1,9 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Taber&units=imperial&appid=502854408fb5e148e3e7e1ca21917b63";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Taber&units=metric&appid=502854408fb5e148e3e7e1ca21917b63";
 
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        //console.log(jsObject);
+        console.log(jsObject);
 
         // Get temp and speed from weather api
         let temp = jsObject.main.temp;
@@ -11,16 +11,16 @@ fetch(apiURL)
         let windChill;
 
         // If/Then for HTML display for calulated wind chill
-        if (temp <= 50 && speed > 3) {
+        if (temp <= 10 && speed > 4.8) {
              windChill = calculation(temp, speed);
-             windChill = `${windChill} &#176;F`;
+             windChill = `${windChill} &#176;C`;
         } else {
              windChill = 'N/A';
         };
 
         // Wind Chill calculation
         function calculation(t, s) {
-            let number = 35.74 + (0.6215 * t) - ((35.75 * s) **  0.16) + ((0.4275 * t * s) ** 0.16);
+            let number = 13.12 + (0.6215 * t) - (11.37 * (s **  0.16)) + ((0.3965 * t) * (s ** 0.16));
             return Math.round(number);
         };
 
