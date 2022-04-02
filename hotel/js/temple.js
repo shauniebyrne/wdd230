e80587b614571a7d7e5e21146326d1f87c63e1c5
shauniebyrne <byr21002@byui.edu>
2022-04-02 04:49:46 -0700

    closer

--- hotel/js/temple.js
+++ hotel/js/temple.js
@@ -19,12 +19,22 @@ function displayCards(card) {
     let p1 = document.createElement('p');
     let p2 = document.createElement('p');
     let p3 = document.createElement('p');
+    let p4 = document.createElement('p');
+    let p5 = document.createElement('p');
+    let p6 = document.createElement('p');
+    let p7 = document.createElement('p');
+    let p8 = document.createElement('p');
 
     // Add textContent to temple card
     h3.textContent = card.name;
     p1.textContent = card.address;
     p2.textContent = card.telephone;
     p3.textContent = card.email;
+    p4.textContent = `Services: ${card.services}`;
+    p5.textContent = `History: ${card.history}`;
+    p6.textContent = `Ordinances Schedule: ${card.ordinanceschedule}`;
+    p7.textContent = `Session Schedule: ${card.sessionschedule}`;
+    p8.textContent = `Closure Schedule: ${card.closureschedule}`;
 
     // Add content to temple image
     templeImg.setAttribute('src', card.img);
@@ -41,43 +51,12 @@ function displayCards(card) {
     section.appendChild(p1);
     section.appendChild(p2);
     section.appendChild(p3);
+    section.appendChild(p4);
+    section.appendChild(p5);
+    section.appendChild(p6);
+    section.appendChild(p7);
+    section.appendChild(p8);
 
     // Append all into parent section
     container.appendChild(section);
-}
-
-section.document.querySelector('.container').addEventListener('click', addInfo);
-
-function addInfo(jasonObject) {
-    const newInfo = jasonObject['temples'];
-
-    newInfo.forEach((card) => {
-        // Create new elements for card once clicked
-        let section2 = document.createElement('section');
-        let p4 = document.createElement('p');
-        let p5 = document.createElement('p');
-        let p6 = document.createElement('p');
-        let p7 = document.createElement('p');
-        let p8 = document.createElement('p');
-
-        // Add textContent
-        p4.textContent = `Services: ${card.services}`;
-        p5.textContent = `History: ${card.history}`;
-        p6.textContent = `Ordinances Schedule: ${card.ordinanceschedule}`;
-        p7.textContent = `Session Schedule: ${card.sessionschedule}`;
-        p8.textContent = `Closure Schedule: ${card.closureschedule}`;
-
-        // Add class to section
-        section2.classList.add('container');
-
-        // Append children to section
-        section2.appendChild(p4);
-        section2.appendChild(p5);
-        section2.appendChild(p6);
-        section2.appendChild(p7);
-        section2.appendChild(p8);
-
-        // Add to div
-        container.appendChild(section2);
-    })
 }
