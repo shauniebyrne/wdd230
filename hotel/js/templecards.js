1bab89b084547bdfcd3cfa30f10e721c4ee561f4
shauniebyrne <byr21002@byui.edu>
2022-04-05 15:13:19 -0700

    so close

--- hotel/js/templecards.js
+++ hotel/js/templecards.js
@@ -14,13 +14,6 @@ fetch(templeURL)
         if (button.innerHTML === "&#9733;") {
             const likeBtn = document.querySelector('.local-storage');
             let likedStorage = window.localStorage.getItem('liked-temple');
-
-            function buttonChanged () {
-                likeBtn.innerHTML = "&#9733;";
-            }
-
-            likeBtn.addEventListener('click', buttonChanged);
-
             // Store clicked button in localstorage
             localStorage.setItem('liked-temple', likeBtn);
         }
